7c2fa0152724523e152492a2f0962da0704b8d53
Kelly Harrison <kelly@sbsoftwareshop.local>
2017-02-09 15:01:52 -0500

    Stuff

--- skeleton-typescript-webpack/src/chores.ts
+++ skeleton-typescript-webpack/src/chores.ts
@@ -1,10 +1,14 @@
 //import {computedFrom} from 'aurelia-framework';
+import * as pouchdb from 'pouchdb';
 
 export class Welcome {
   heading: string = 'Welcome to the Awesome Navigation App';
   firstName: string = 'John';
   lastName: string = 'Doe';
   previousValue: string = this.fullName;
+
+  db = new PouchDB('urchores');
+
 
   //Getters can't be directly observed, so they must be dirty checked.
   //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
